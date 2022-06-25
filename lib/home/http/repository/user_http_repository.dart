865e6c5e29bde2adcadfa130379dd0bc863a9e56
RoAlencar <roalencarx@gmail.com>
2022-06-25 09:29:33 -0300

    Criando factory para consumir api

--- lib/home/http/repository/user_http_repository.dart
+++ lib/home/http/repository/user_http_repository.dart
@@ -1,8 +1,21 @@
+import 'dart:convert';
+
 import 'package:consumindo_api_com_flutter/model/user_model.dart';
 import 'package:consumindo_api_com_flutter/repository/i_user_repository.dart';
 import 'package:http/http.dart' as http;
 
 class UserHttpRepository implements IUserRepository {
   @override
-  Future<List<UserModel>> findAllUsers() {}
+  Future<List<UserModel>> findAllUsers() async {
+    final response =
+        await http.get('https://sujeitoprogramador.com/r-api/?api=filmes');
+    final List<Map<String, dynamic>> responseMap = jsonDecode(response.body);
+    responseMap.map<UserModel>((resp) {
+      final model = UserModel(
+          id: resp['id'] as String,
+          nome: resp['nome'] as String,
+          sinopse: resp['sinopse'] as String,
+          foto: resp['foto'] as String);
+    }).toList();
+  }
 }
