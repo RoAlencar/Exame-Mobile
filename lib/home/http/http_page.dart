5b2650c8e4e7b125bfe46099451f7a45f01b72bb
RoAlencar <roalencarx@gmail.com>
2022-06-25 09:53:05 -0300

    StateMixin

--- lib/home/http/http_page.dart
+++ lib/home/http/http_page.dart
@@ -1,6 +1,8 @@
+import 'package:consumindo_api_com_flutter/home/http/http_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class HttpPage extends StatelessWidget {
+class HttpPage extends GetView<HttpController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
